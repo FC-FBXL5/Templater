b1359118ae6fc1ccafb2f2567c1b8106c2322dd3
FC-FBXL5 <103560699+FC-FBXL5@users.noreply.github.com>
2025-07-19 08:03:07 +0200

    Update Init.py

--- Init.py
+++ Init.py
@@ -1,6 +1,28 @@
-#! python
 # -*- coding: utf-8 -*-
-# (c) 2025 <FBXL5>, LGPL-2.0-or-later as FreeCAD
+# ***************************************************************************
+# *                                                                         *
+# *   InitGui.py                                                            *
+# *                                                                         *
+# *   Copyright (c) 2025 FBXL5                                              *
+# *                                                                         *
+# *   This program is free software; you can redistribute it and/or modify  *
+# *   it under the terms of the GNU Lesser General Public License (LGPL)    *
+# *   as published by the Free Software Foundation; either version 2 of     *
+# *   the License, or (at your option) any later version.                   *
+# *   for detail see the LICENCE text file.                                 *
+# *                                                                         *
+# *   This program is distributed in the hope that it will be useful,       *
+# *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+# *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+# *   GNU Library General Public License for more details.                  *
+# *                                                                         *
+# *   You should have received a copy of the GNU Library General Public     *
+# *   License along with this program; if not, write to the Free Software   *
+# *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  *
+# *   USA                                                                   *
+# *                                                                         *
+# ***************************************************************************
+"""A workbench related to TechDraw templates, views, and symbols."""
 
 # FreeCAD init script of the Templater module
 
