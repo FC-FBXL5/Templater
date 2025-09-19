b76927465fad863e091d12c2397ecaf3db306a33
FC-FBXL5 <103560699+FC-FBXL5@users.noreply.github.com>
2025-09-19 15:18:49 +0200

    Update Templater.ts
    Filled with up-to-date content

--- Resources/translations/Templater.ts
+++ Resources/translations/Templater.ts
@@ -1,1 +1,399 @@
-
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>Templater</name>
+    <message>
+        <location filename="../InitGui.py" line="52"/>
+        <location filename="../InitGui.py" line="78"/>
+        <location filename="../InitGui.py" line="96"/>
+        <source>Templater</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../InitGui.py" line="56"/>
+        <source>Some tools to create auxiliary views, templates, and symbols</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../TemplaterAuxViewCmd.py" line="110"/>
+        <source>Auxiliary view settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../TemplaterAuxViewCmd.py" line="113"/>
+        <source>Marker of the auxiliary view: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../TemplaterAuxViewCmd.py" line="114"/>
+        <source>Direction: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../TemplaterAuxViewCmd.py" line="115"/>
+        <source>Reverse direction</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../TemplaterAuxViewCmd.py" line="117"/>
+        <source>Along edge</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../TemplaterAuxViewCmd.py" line="120"/>
+        <source>Sets the view marker</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../TemplaterAuxViewCmd.py" line="123"/>
+        <source>Reverses the view direction</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../TemplaterAuxViewCmd.py" line="128"/>
+        <source>If checked the line of sight will be aligned with the edge 
+instead of being perpendicular to it</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../TemplaterAuxViewCmd.py" line="235"/>
+        <source>No active document available!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../TemplaterTemplateMultiCmd.py" line="69"/>
+        <source>There is no active document!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../TemplaterTemplateMultiCmd.py" line="468"/>
+        <location filename="../TemplaterTemplateWikiCmd.py" line="266"/>
+        <source>Template settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../TemplaterTemplateMultiCmd.py" line="472"/>
+        <source>Select the desired 
+sheet format</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../TemplaterTemplateMultiCmd.py" line="475"/>
+        <source>Does the drawing need a Frame?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../TemplaterTemplateMultiCmd.py" line="478"/>
+        <source>Does the frame require zone indices?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../TemplaterTemplateMultiCmd.py" line="481"/>
+        <source>Should the upper and right indices be tilted?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../TemplaterTemplateMultiCmd.py" line="484"/>
+        <source>Should the template integrate a title block?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../TemplaterTemplateMultiCmd.py" line="487"/>
+        <source>Do text entries require a different color?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../TemplaterTemplateMultiCmd.py" line="491"/>
+        <source>Enter the number of rows if the template 
+requires a bill of material?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../TemplaterTemplateMultiCmd.py" line="495"/>
+        <source>Should the created template be inserted in 
+the active document to add a new page?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../TemplaterTemplateMultiCmd.py" line="499"/>
+        <source>Should a symbol containing a title block 
+be added to the new page?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../TemplaterTemplateMultiCmd.py" line="503"/>
+        <source>Don&apos;t forget to save, close and reopen the file 
+before insering another template!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../TemplaterTemplateMultiCmd.py" line="507"/>
+        <source>Selects a sheet format for the new page and
+resets the number of BOM rows to 0, if changed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../TemplaterTemplateMultiCmd.py" line="508"/>
+        <source>Draw a Frame</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../TemplaterTemplateMultiCmd.py" line="511"/>
+        <source>Adds a frame to the new page</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../TemplaterTemplateMultiCmd.py" line="512"/>
+        <source>Place zone indices</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../TemplaterTemplateMultiCmd.py" line="515"/>
+        <source>Adds zone indices and separators to the frame</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../TemplaterTemplateMultiCmd.py" line="516"/>
+        <source>Tilt indices 90° ccw</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../TemplaterTemplateMultiCmd.py" line="519"/>
+        <source>Tilts upper and right indices to be readable from the right</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../TemplaterTemplateMultiCmd.py" line="520"/>
+        <source>Add a title block</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../TemplaterTemplateMultiCmd.py" line="523"/>
+        <source>Adds a titleblock with some editable texts to the new page</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../TemplaterTemplateMultiCmd.py" line="524"/>
+        <source>Change to ink-blue</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../TemplaterTemplateMultiCmd.py" line="527"/>
+        <source>Colors editable texts in ink-blue</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../TemplaterTemplateMultiCmd.py" line="533"/>
+        <source>Enter the number 
+of BOM rows or use 
+right mouse buton 
+to select &quot;None&quot; or 
+&quot;Maximum&quot;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../TemplaterTemplateMultiCmd.py" line="534"/>
+        <source>None</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../TemplaterTemplateMultiCmd.py" line="535"/>
+        <source>Standard (2)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../TemplaterTemplateMultiCmd.py" line="536"/>
+        <source>Maximum</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../TemplaterTemplateMultiCmd.py" line="537"/>
+        <source>Insert template</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../TemplaterTemplateMultiCmd.py" line="541"/>
+        <source>Inserts the saved template into the active document 
+to create a new page</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../TemplaterTemplateMultiCmd.py" line="544"/>
+        <source>Insert title block symbol</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../TemplaterTemplateMultiCmd.py" line="547"/>
+        <source>Inserts a title block as a symbols into the new page</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../TemplaterTemplateMultiCmd.py" line="550"/>
+        <source>Selects a template coded after one of Benjamin May&apos;s proposals</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../TemplaterTemplateMultiCmd.py" line="551"/>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../TemplaterTemplateMultiCmd.py" line="553"/>
+        <source>OK</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../TemplaterTemplateMultiCmd.py" line="749"/>
+        <source>New Template Multi</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../TemplaterTemplateMultiCmd.py" line="755"/>
+        <source>Creates and inserts a new template
+in the active document</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../TemplaterTemplateWikiCmd.py" line="264"/>
+        <source>Color of text entries</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../TemplaterTemplateWikiCmd.py" line="265"/>
+        <source>Ink-blue</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../TemplaterTemplateWikiCmd.py" line="270"/>
+        <source>Don&apos;t forget to Save, Close 
+and Reopen the file!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../TemplaterTemplateWikiCmd.py" line="274"/>
+        <source>Check for ink blue text entries</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../TitleBlock_KG.py" line="40"/>
+        <source>Drawn:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../TitleBlock_KG.py" line="41"/>
+        <source>Approved:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../TitleBlock_KG.py" line="42"/>
+        <source>Name:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../TitleBlock_KG.py" line="43"/>
+        <source>CAD version:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../TitleBlock_KG.py" line="44"/>
+        <source>Date:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../TitleBlock_KG.py" line="45"/>
+        <source>Title:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../TitleBlock_KG.py" line="46"/>
+        <source>Part number:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../TitleBlock_KG.py" line="47"/>
+        <source>Scale:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../TitleBlock_KG.py" line="48"/>
+        <source>Material:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../TitleBlock_KG.py" line="49"/>
+        <source>Mass:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../TitleBlock_KG.py" line="50"/>
+        <source>Format:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../TitleBlock_KG.py" line="51"/>
+        <source>Sheet:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../TitleBlock_KG.py" line="54"/>
+        <source>Owner:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../TitleBlock_KG.py" line="59"/>
+        <source>Pos.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../TitleBlock_KG.py" line="60"/>
+        <source>Amount</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../TitleBlock_KG.py" line="61"/>
+        <source>Unit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../TitleBlock_KG.py" line="62"/>
+        <source>Title</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../TitleBlock_KG.py" line="63"/>
+        <source>Part number</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../TitleBlock_KG.py" line="64"/>
+        <source>Material</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../TitleBlock_KG.py" line="65"/>
+        <source>Mass</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../TitleBlock_KG.py" line="68"/>
+        <source>Remark</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Templater_AuxView</name>
+    <message>
+        <location filename="../TemplaterAuxViewCmd.py" line="428"/>
+        <source>Auxiliary view</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../TemplaterAuxViewCmd.py" line="437"/>
+        <source>Creates an auxiliary view
+1. Select 2 vertices or 1 edge.
+2. Invoke this Command</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
